b84820afcf57ca002d43455ce163c0be0eba10fe
SneK152 <kakanisnehil@gmail.com>
2021-07-05 11:17:44 -0700

    Added 5 gems but have not created models and textures yet

---
+++ src/main/java/com/snek152/reworks/registry/ModBlocks.java
@@ -0,0 +1,14 @@
+package com.snek152.reworks.registry;
+
+import net.minecraft.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class ModBlocks {
+
+}
