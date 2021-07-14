d7fc3612e7713614f2d7270365628f15cdbaf8fd
SneK152 <kakanisnehil@gmail.com>
2021-07-14 12:48:21 -0700

    Reducing shield activation (beacon) sound

--- src/main/java/com/snek152/reworks/item/Topaz.java
+++ src/main/java/com/snek152/reworks/item/Topaz.java
@@ -22,15 +22,10 @@ public class Topaz extends Item {
     public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
         playerIn.addEffect(new EffectInstance(Effects.DAMAGE_RESISTANCE,30,2,false,true,true));
         playerIn.addEffect(new EffectInstance(Effects.GLOWING,10,0,false,false,false));
-        worldIn.playSound(playerIn,playerIn, SoundEvents.BEACON_AMBIENT, SoundCategory.AMBIENT,1,1);
+        worldIn.playSound(playerIn,playerIn, SoundEvents.BEACON_AMBIENT, SoundCategory.AMBIENT,0.5f,1);
 //        if (playerIn.getMainHandItem().getItem() == ModItems.TOPAZ.get() || playerIn.getOffhandItem().getItem() == ModItems.TOPAZ.get()) {
 //            playerIn.addEffect(new EffectInstance(Effects.DAMAGE_RESISTANCE,0,1,false,false,true));
 //        }
         return ActionResult.pass(playerIn.getItemInHand(handIn));
-    }
-
-    @Override
-    public void onUseTick(World world, LivingEntity entity, ItemStack stack, int p_219972_4_) {
-        world.playSound((PlayerEntity) entity,entity, SoundEvents.BEACON_AMBIENT, SoundCategory.AMBIENT,1,1);
     }
 }
