aa549e8ab6891f31cfe5b2261e7afc638b89ee04
SneK152 <kakanisnehil@gmail.com>
2021-07-13 17:53:42 -0700

    Remapping the project

--- src/main/java/com/snek152/reworks/Reworks.java
+++ src/main/java/com/snek152/reworks/Reworks.java
@@ -8,16 +8,13 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,15 +22,14 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Reworks.MOD_ID)
-public class Reworks {
+public class Reworks
+{
 
     public static final String MOD_ID = "reworks";
-
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Reworks() {
-
         Registration.register();
         ModItems.register();
         // Register the setup method for modloading
@@ -45,12 +41,12 @@ public class Reworks {
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
+    private void setup(final FMLCommonSetupEvent event)
+    {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
@@ -58,24 +54,22 @@ public class Reworks {
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("reworks", "helloworld", () -> {
-            LOGGER.info("Hello world from the MDK");
-            return "Hello world";
-        });
+        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
-    private void processIMC(final InterModProcessEvent event) {
+    private void processIMC(final InterModProcessEvent event)
+    {
         // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m -> m.getMessageSupplier().get()).
+                map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
@@ -85,7 +79,7 @@ public class Reworks {
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
--- src/main/java/com/snek152/reworks/item/ModItems.java
+++ src/main/java/com/snek152/reworks/item/ModItems.java
@@ -1,5 +1,13 @@
 package com.snek152.reworks.item;
 
+import com.snek152.reworks.util.Registration;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
+import net.minecraftforge.fml.RegistryObject;
+
 public class ModItems {
+    public static final RegistryObject<Item> AQUAMARINE = Registration.ITEMS.register("aquamarine",
+            () -> new Item(new Item.Properties().rarity(Rarity.UNCOMMON).food(new Food.Builder().build())));
     public static void register() {}
 }
